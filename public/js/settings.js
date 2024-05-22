a1db0a2b7778703d66feea445a2977f78cc755ab
bas1c <asad@saleems.com>
2024-05-23 00:23:39 +0100

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -32,7 +32,7 @@ var tabPresets = {
         icon: 'https://ssl.gstatic.com/classroom/ic_product_classroom_32.png'
     },
     default: {
-        name: 'Art Class',
+        name: 'SchoolisFun',
         icon: '/assets/images/icon.png'
     }
 }
